37c4985dcc24d1689cefb37a40da6b5e51a94f5f
magodo <wztdyl@sina.com>
2022-07-05 14:49:13 +0800

    New func: Query with import specification

--- aztft/aztft.go
+++ aztft/aztft.go
@@ -15,6 +15,37 @@ import (
 // It firstly statically search the known resource mappings. If there are multiple matches and the "allowAPI" is true,
 // it will further call Azure API to retrieve additionl information about this resource and return the exact match.
 func Query(idStr string, allowAPI bool) ([]string, error) {
+	l, err := query(idStr, allowAPI)
+	if err != nil {
+		return nil, err
+	}
+	out := make([]string, len(l))
+	for i := range l {
+		out[i] = l[i].ResourceType
+	}
+	return out, nil
+}
+
+func QueryImportSpecs(idStr string, allowAPI bool) ([]string, []string, error) {
+	l, err := query(idStr, allowAPI)
+	if err != nil {
+		return nil, nil, err
+	}
+	id, _ := armid.ParseResourceId(idStr)
+	var outRts, outImports []string
+	for _, item := range l {
+		outRts = append(outRts, item.ResourceType)
+		if rid, ok := id.(*armid.ScopedResourceId); ok {
+			if err := rid.Normalize(item.ImportSpec); err != nil {
+				return nil, nil, fmt.Errorf("failed to normalize id for type %s, id: %s", item.ResourceType, id)
+			}
+		}
+		outImports = append(outImports, id.String())
+	}
+	return outRts, outImports, nil
+}
+
+func query(idStr string, allowAPI bool) ([]resmap.ARMId2TFMapItem, error) {
 	id, err := armid.ParseResourceId(idStr)
 	if err != nil {
 		return nil, fmt.Errorf("invalid resource id: %v", err)
@@ -49,10 +80,10 @@ func Query(idStr string, allowAPI bool) ([]string, error) {
 		l = []resmap.ARMId2TFMapItem{*item}
 	}
 
-	var out []string
-	for _, item := range l {
-		out = append(out, item.ResourceType)
-	}
-	sort.Strings(out)
-	return out, nil
+	sort.Slice(l, func(i, j int) bool {
+		return l[i].ResourceType < l[j].ResourceType
+	})
+
+	return l, nil
+
 }
--- main.go
+++ main.go
@@ -14,28 +14,44 @@ func usage() {
 
 Options:
 
-  -api: (Default: false) Allow to use Azure API to disambiguate matching results (e.g. whether a VM is a Linux VM or Windows VM)
+  -api		: (Default: false) Allow to use Azure API to disambiguate matching results (e.g. whether a VM is a Linux VM or Windows VM)
+  -import	: (Default: false) Print the TF import instruction
 `)
 }
 
 func main() {
 	flagAPI := flag.Bool("api", false, "")
+	flagImport := flag.Bool("import", false, "")
 	flag.Usage = usage
 	flag.Parse()
 	if len(flag.Args()) != 1 {
 		usage()
 		os.Exit(1)
 	}
-	rts, err := aztft.Query(flag.Args()[0], *flagAPI)
-	if err != nil {
-		log.Fatal(err)
+	var output []string
+	if *flagImport {
+		rts, specs, err := aztft.QueryImportSpecs(flag.Args()[0], *flagAPI)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < len(rts); i++ {
+			output = append(output, fmt.Sprintf("terraform import %s %s", rts[i], specs[i]))
+		}
+	} else {
+		rts, err := aztft.Query(flag.Args()[0], *flagAPI)
+		if err != nil {
+			log.Fatal(err)
+		}
+		output = rts
 	}
-	switch len(rts) {
+	switch len(output) {
 	case 0:
 		fmt.Println("No match")
 	case 1:
-		fmt.Println(rts[0])
+		fmt.Println(output[0])
 	default:
-		fmt.Println(rts)
+		for _, line := range output {
+			fmt.Println(line)
+		}
 	}
 }
