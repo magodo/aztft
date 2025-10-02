a4830d217749aa2e92d183822498226ed57c4d59
magodo <wztwcy@gmail.com>
2025-10-03 07:41:46 +0800

    Support v4.47.0

--- internal/tfid/tfid.go
+++ internal/tfid/tfid.go
@@ -168,7 +168,8 @@ func StaticBuild(id armid.ResourceId, rt string) (string, error) {
 		"azurerm_subnet_network_security_group_association",
 		"azurerm_subnet_route_table_association",
 		"azurerm_mssql_job_schedule",
-		"azurerm_stream_analytics_job_storage_account":
+		"azurerm_stream_analytics_job_storage_account",
+		"azurerm_api_management_workspace_policy":
 		id = id.Parent()
 
 	// Parent Scope
